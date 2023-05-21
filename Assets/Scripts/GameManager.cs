98793d128cffba280959ba6fdb812c679a30687c
Emanuel Vargas <reponzo01@gmail.com>
2023-05-21 12:13:05 -0400

    pair matching functionality and particle effects

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance is null)
+                Debug.LogError("Game Manager is NULL");
+
+            return _instance;
+        }
+    }
+
+    [SerializeField]
+    private List<Material> emojis;
+
+    [SerializeField]
+    private GameObject mainCube4x4;
+
+    [SerializeField]
+    private int maxFlippedTilesAllowed = 2;
+
+    private List<Tile> _flippedTiles;
+
+    private void Awake()
+    {
+        _instance = this;
+
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = 60;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _flippedTiles = new List<Tile>();
+
+        List<GameObject> activePanels = GetActivePanels(mainCube4x4);
+        if (activePanels.Count > 0)
+        {
+            var allActiveTiles = new List<GameObject>();
+            foreach (var panel in activePanels)
+            {
+                allActiveTiles.AddRange(GetActiveTiles(panel));
+            }
+
+            var numEmojisToSelect = Mathf.Ceil(allActiveTiles.Count / 2);
+            if (numEmojisToSelect > 0)
+            {
+                emojis.Shuffle();
+                var tileIndecies = new List<int>();
+                for (var i = 0; i < allActiveTiles.Count; i++)
+                {
+                    tileIndecies.Add(i);
+                }
+                tileIndecies.Shuffle();
+
+                var emojiIndex = 0;
+                foreach (var index in tileIndecies)
+                {
+                    if (emojiIndex >= numEmojisToSelect)
+                    {
+                        emojiIndex = 0;
+                    }
+                    var tile = allActiveTiles[index].GetComponent<Tile>();
+                    if (tile != null)
+                    {
+                        tile.SetEmojiMaterial(emojis[emojiIndex]);
+                    }
+                    emojiIndex++;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private List<GameObject> GetActivePanels(GameObject cube)
+    {
+        var activePanels = new List<GameObject>();
+        for (var i = 0; i < cube.transform.childCount; i++)
+        {
+            var child = cube.transform.GetChild(i);
+            if (child.CompareTag("Panel") && child.gameObject.activeSelf)
+            {
+                activePanels.Add(child.gameObject);
+            }
+        }
+        return activePanels;
+    }
+
+    private List<GameObject> GetActiveTiles(GameObject panel)
+    {
+        var activeTiles = new List<GameObject>();
+        for (var i = 0; i < panel.transform.childCount; i++)
+        {
+            var child = panel.transform.GetChild(i);
+            if (child.CompareTag("Tile") && child.gameObject.activeSelf)
+            {
+                activeTiles.Add(child.gameObject);
+            }
+        }
+        return activeTiles;
+    }
+
+    IEnumerator ResetTiles()
+    {
+        yield return new WaitForSeconds(1f);
+        _flippedTiles[0].HideTile();
+        _flippedTiles[1].HideTile();
+        _flippedTiles.Clear();
+    }
+
+    IEnumerator ExplodeTiles()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _flippedTiles[0].ExplodeTile();
+        _flippedTiles[1].ExplodeTile();
+        _flippedTiles.Clear();
+    }
+
+    public void TileClicked(Tile tile)
+    {
+        if (_flippedTiles.Count < maxFlippedTilesAllowed)
+        {
+            tile.ShowTile();
+            _flippedTiles.Add(tile);
+            // TODO: Technically, the following code does not support more than 2 max tiles flipped allowed
+            if (_flippedTiles.Count == maxFlippedTilesAllowed)
+            {
+                if (!_flippedTiles[0].GetEmojiName().Equals(_flippedTiles[1].GetEmojiName()))
+                {
+                    StartCoroutine(ResetTiles());
+                }
+                else
+                {
+                    StartCoroutine(ExplodeTiles());
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private string _emojiName;
+    private bool _isShown;
+    private bool _isExploding;
+    private Quaternion _originalTileRotation;
+    private string _emojiGameObjectName = "Emoji";
+    private string _explosionGameObjectName = "Explosion";
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _isShown = false;
+        _originalTileRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // 0 = facing front. 180 = facing back.
+        if (_isShown && transform.rotation.eulerAngles.y > 0)
+        {
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z), 600f * Time.deltaTime);
+        }
+
+        if (!_isShown && transform.rotation.eulerAngles.y < 180)
+        {
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, _originalTileRotation, 600f * Time.deltaTime);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!_isExploding)
+        {
+            GameManager.Instance.TileClicked(this);
+        }
+    }
+
+    public string GetEmojiName()
+    {
+        return _emojiName;
+    }
+
+    public void SetEmojiMaterial(Material emojiMat)
+    {
+        var renderer = transform.Find(_emojiGameObjectName).GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material = emojiMat;
+            _emojiName = emojiMat.name.Replace("(Instance)", "");
+        }
+    }
+
+    public void ShowTile()
+    {
+        _isShown = true;
+    }
+
+    public void HideTile()
+    {
+        _isShown = false;
+    }
+
+    public void ExplodeTile()
+    {
+        _isExploding = true;
+        var tileMeshRenderer = transform.GetComponent<MeshRenderer>();
+        var emojiMeshRenderer = transform.Find(_emojiGameObjectName).GetComponent<MeshRenderer>();
+        var explosion = transform.Find(_explosionGameObjectName).GetComponent<ParticleSystem>();
+        if (explosion != null)
+        {
+            explosion.Play();
+        }
+
+        if (emojiMeshRenderer != null) emojiMeshRenderer.enabled = false;
+        if (tileMeshRenderer != null) tileMeshRenderer.enabled = false;
+        Destroy(gameObject, 1.1f);
+    }
+}
